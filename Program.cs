20881279be2a84c378bb9be5c6a43395e5ca032b
vityavic <97730957+vityavic@users.noreply.github.com>
2022-09-15 09:11:31 +0200

    objeckt szamolas

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class szamol
+    {
+        public static int darab = 0;
+        public static int maxdarab = 0;
+        public szamol()
+        {
+            darab++;
+            Console.WriteLine(darab);
+        }
+        ~szamol()
+        {
+            darab--;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 500000; i++)
+            {
+                new szamol();
+            }
+        }
+    }
+}
